2b19aeca14eeeedd44ef85382523a1899e2ffbbc
diogenesjup <diogenesjunior.ti@gmail.com>
2020-07-06 15:30:27 -0300

    atualizações 06 de julho

--- view/View.js
+++ view/View.js
@@ -2,7 +2,7 @@ class View{
      
      constructor(idEvento){
          
-         this._idEvento = idEvento;
+         //this._idEvento = idEvento;
          this._salaActive = $("#salaActive");
          this._programacao = $("#programacao");
          this._scroll = $("#scroll");
@@ -11,12 +11,24 @@ class View{
          this._alertaRetorno = $("#alertaRetorno");
          this._loginaovivo = $("section.login-aovivo");
 
+         let url_string = window.location.href;
+         let url = new URL(url_string);
+         let c = url.searchParams.get("c");
+         console.log("URL ID: "+c);
+
+         this._idEvento = c;
+
      }
       
 
      carregamento(){
      	console.log("EXIBINDO CARREGAMENTO");
-     	this._alertaRetorno.html(`<div class="loader loader--style2" title="1"> <svg version="1.1" id="loader-1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="40px" height="40px" viewBox="0 0 50 50" style="enable-background:new 0 0 50 50;" xml:space="preserve"> <path fill="#000" d="M25.251,6.461c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615V6.461z"> <animateTransform attributeType="xml" attributeName="transform" type="rotate" from="0 25 25" to="360 25 25" dur="0.6s" repeatCount="indefinite"/> </path> </svg> </div>`);
+     	this._alertaRetorno.html(`<p style="text-align: center;">
+             <img src="images/icone.png" class="ld ld-bounce" alt="BeautyConnect" style="width:62px;margin-top:32px;animation-duration: 0.65s;">
+           </p>
+           <p style="text-align: center;font-size: 14px;">
+             Carregando
+           </p>`);
      }
      
 
@@ -46,6 +58,80 @@ class View{
                   	 scopo.loginSucesso();
                   	 localStorage.setItem("evento",JSON.stringify(dados.evento));
                   	 localStorage.setItem("aovivo",JSON.stringify(dados.aovivo));
+                     localStorage.setItem("logadoAoVivo","sim");
+                     
+
+                     $("#salaActive").html(`
+
+                         <h2>${dados.aovivo[0].nome_sala}</h2>
+                         <a href="javascript:void(0)" onclick="app.verSala(${dados.aovivo[0].id})" title="ASSISTIR PALESTRA AO VIVO">
+                            ASSISTIR PALESTRA AO VIVO
+                         </a>
+
+
+                      `);
+
+                     $("#programacao").html(`
+
+                          <h2 class="titulo">Programação - ${dados.aovivo[0].nome_sala}</h2>
+                          <div class="scroll" id="scroll">
+                             
+                             ${dados.aovivo[0].programacao}
+
+                          </div>
+
+                      `);
+
+
+                     $("#listaSalas").html("");
+
+                     for(var i=0;i<dados.aovivo.length;i++){
+                       
+                       $("#listaSalas").append(`
+                            
+                              <!-- SALA -->
+                              <div class="sala">
+                                 <h2>${dados.aovivo[i].nome_sala}</h2>
+                                 <a href="javascript:void(0)" onclick="app.ativarPreviewSala(${dados.aovivo[i].id})" title="${dados.aovivo[i].nome_sala}">
+                                    ${dados.aovivo[i].tema}
+                                 </a>
+                              </div>
+                              <!-- SALA -->
+
+                       `);
+
+                     }
+
+
+                     $("#listaExpositores").html();
+
+                     for(var i = 0;i<dados.aovivo_expositores.length;i++){
+
+                      $("#listaExpositores").append(`
+                           
+                           <!-- EXPOSITOR -->
+                           <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-12 expositor">
+                              <div class="logo-expositor" style="background:url('https://beautyconnect.com.br/cdn/${dados.aovivo_expositores[i].logo}') transparent no-repeat;background-size: 200px auto;background-position: center center;">
+                                 &nbsp;
+                              </div>
+                              <p>
+                                <a href="${dados.aovivo_expositores[i].url_apresentacao}" title="Apresentação" class="btn btn-default" target="_blank">
+                                  APRESENTAÇÃO
+                                </a>
+                              </p>
+                              <p>
+                                <a href="${dados.aovivo_expositores[i].url_atendimento}" title="Atendimento" class="btn btn-default" target="_blank">
+                                  ATENDIMENTO
+                                </a>
+                              </p>
+                           </div>
+                           <!-- EXPOSITOR -->
+
+                      `);
+
+                     }
+
+
                   }else{
                   	scopo.loginInvalido();
                   }
@@ -58,6 +144,53 @@ class View{
               // FINAL CHAMADA AJAX
 
      }
+
+
+
+     verSala(idConteudo){
+         
+         console.log("INICIANDO MÉTODO PARA VER SALA: "+idConteudo);
+         window.open("aovivo.html?c="+idConteudo, '_blank');
+     
+     }
+
+
+     ativarPreviewSala(idConteudo){
+
+         console.log("INICIANDO MÉTODO PARA VER PREVIEW DA SALA"+idConteudo);
+
+         let conteudos = JSON.parse(localStorage.getItem("aovivo"));
+         
+         console.log("CONTEÚDO SALVO:");
+         console.log(conteudos);
+
+         for(var i = 0;i<conteudos.length;i++){
+              
+              if(conteudos[i].id==idConteudo){
+
+                   $("#salaActive").html(`
+                         <h2>${conteudos[i].nome_sala}</h2>
+                         <a href="javascript:void(0)" onclick="app.verSala(${conteudos[i].id})" title="ASSISTIR PALESTRA AO VIVO">
+                            ASSISTIR PALESTRA AO VIVO
+                         </a>
+                    `);
+
+                   $("#programacao").html(`
+                          <h2 class="titulo">Programação - ${conteudos[i].nome_sala}</h2>
+                          <div class="scroll" id="scroll">
+                             ${conteudos[i].programacao}
+                          </div>
+                   `);
+
+              }
+
+         }
+                      
+
+     }
+
+
+
 
      loginSucesso(){
      
